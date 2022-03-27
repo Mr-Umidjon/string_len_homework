049d993a3c1e27e9e4c116512c1fa728b81503ca
Umidjon <mrumidjon2020@gmail.com>
2022-03-27 15:42:25 +0500

    Given three strings, s1, s2 and s3. return their odd lengths 01

--- string_len07.py
+++ string_len07.py
@@ -1,4 +1,4 @@
-def main(s1,s2,s3):
+def main(s1, s2, s3):
     """
     Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
     Args:
@@ -8,4 +8,12 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    ans = "["
+    if len(s1) % 2 == 1:
+        ans += s1
+    if len(s2) % 2 == 1:
+        ans += ", " + s2
+    if len(s2) % 2 == 1:
+        ans += ", " + s3
+    ans += "]"
+    return ans
