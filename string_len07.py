84a27a4005a6c996df458d5d43c327c986b8c37b
Umidjon <mrumidjon2020@gmail.com>
2022-03-29 19:11:35 +0500

    Given three strings, s1, s2 and s3. return their odd lengths 01

--- string_len07.py
+++ string_len07.py
@@ -1,6 +1,6 @@
 def main(s1, s2, s3):
     """
-    Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
+    g
     Args:
         s1: string
         s2: string
@@ -8,28 +8,24 @@ def main(s1, s2, s3):
     Returns:
         string
     """
-
-
-
-
     l1 = len(s1) % 2
     l2 = len(s2) % 2
     l3 = len(s3) % 2
     length = l1 + l2 + l3
-    # if l1 + l2 + l3 == 0:
-    #     return '[]'
-    # if l1 and l2 and l3:
-    #     ans = f"[{s1}, {s2}, {s3}]"
-    # elif l1 and l2:
-    #     ans = f"[{s1}, {s2}]"
-    # elif l1 and l3:
-    #     ans = f"[{s1}, {s3}]"
-    # elif l2 and l3:
-    #     ans = f"[{s2}, {s3}]"
-    # else:
-    #     ans = f"[{s1 * l1}{s2 * l2}{s3 * l3}]"
+    if l1 + l2 + l3 == 0:
+        return '[]'
+    if l1 and l2 and l3:
+        ans = f"[{s1}, {s2}, {s3}]"
+    elif l1 and l2:
+        ans = f"[{s1}, {s2}]"
+    elif l1 and l3:
+        ans = f"[{s1}, {s3}]"
+    elif l2 and l3:
+        ans = f"[{s2}, {s3}]"
+    else:
+        ans = f"[{s1 * l1}{s2 * l2}{s3 * l3}]"
 
-    # return ans
+    return ans
 
 
 
